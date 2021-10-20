f8ae93267ae3a1fef2edf50ab9fa3dc806954e96
Juliana Akemi <juliana.kondo@possible.com>
2021-10-20 09:55:22 -0300

    fix: removed margin-top from input

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -14,7 +14,6 @@ const Search = styled.div(
   ({ theme }) => `
     width: 100%;
     height: 48px;
-    margin-top: 40px;
     padding: 12px 16px;
     border: 1px solid ${theme.colors.outline};
     background-color: ${theme.colors.elements};
