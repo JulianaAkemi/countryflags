66eed9a2b330c4d4acc21cfcec479ceddf07ef78
Juliana Akemi <juliana.kondo@possible.com>
2021-10-22 15:47:00 -0300

    fix: added data validation for rendering Card component contents

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 import noImage from '../../assets/no-image-available.png';
 
 const Card = ({ data }) => {
+  if (!!data?.length) return <></>;
+
   return (
     <div>
       {data.map((item) => (
