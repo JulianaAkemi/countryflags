548f4baa3ac38041b282265b369bf004fb6eb468
Juliana Akemi <juliana.kondo@possible.com>
2021-11-04 16:07:52 -0300

    feature(SearchCountries): added functionality to search for a country on search bar

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,18 +1,60 @@
 import styled from 'styled-components';
 import { GrSearch } from 'react-icons/gr';
 import { FormField } from '../FormField';
+import { useState } from 'react';
 
-const SearchBar = () => {
+const SearchBar = ({ propmt, data, getQuery }) => {
+  const [filteredData, setFilteredData] = useState([]);
+
+  const handleFilter = (e) => {
+    const searchWord = e.target.value;
+    const newFilter = data.filter((value) => {
+      return value.title.toLowerCase().includes(searchWord.toLowerCase());
+    });
+    if (searchWord === '') {
+      setFilteredData([]);
+    } else {
+      setFilteredData(newFilter);
+    }
+  };
+
+  const onChange = (q) => {
+    getQuery(q);
+  };
+
   return (
-    <Search>
-      <Icon as={GrSearch} />
-      <input type='text' placeholder='Search for a country…' />
-    </Search>
+    <StyledSearch>
+      <Search>
+        <Icon as={GrSearch} />
+        <input
+          type='text'
+          placeholder={propmt}
+          onChange={(handleFilter, (e) => onChange(e.target.value))}
+        />
+      </Search>
+      {filteredData.lenght != 0 ? (
+        <DataResult>
+          {filteredData.slice(0, 15).map((value) => {
+            return <a key={value.id}>{value.title}</a>;
+          })}
+        </DataResult>
+      ) : (
+        <></>
+      )}
+    </StyledSearch>
   );
 };
 
+const StyledSearch = styled.div`
+  position: relative;
+`;
+
 const Search = styled(FormField)`
   padding: 12px 16px;
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.desktop}) {
+    width: 100%;
+  }
 `;
 
 const Icon = styled.svg`
@@ -20,6 +62,36 @@ const Icon = styled.svg`
 
   path {
     stroke: ${({ theme }) => theme.colors.text};
+  }
+`;
+
+const DataResult = styled.div`
+  width: 100%;
+  color: ${({ theme }) => theme.colors.text};
+  border: 1px solid ${({ theme }) => theme.colors.outline};
+  background-color: ${({ theme }) => theme.colors.elements};
+  color: ${({ theme }) => theme.colors.inputText};
+  font-size: ${({ theme }) => theme.fontSizes.md};
+  font-weight: 400;
+  overflow: hidden;
+  overflow-y: auto;
+  display: flex;
+  flex-direction: column;
+
+  max-height: 200px;
+  position: absolute;
+  z-index: 2;
+
+  a {
+    width: 100%;
+    height: 30px;
+    padding: 6px 0 6px 24px;
+
+    &:hover {
+      background: ${({ theme }) => theme.colors.inputText};
+      color: ${({ theme }) => theme.colors.elements};
+      cursor: pointer;
+    }
   }
 `;
 
